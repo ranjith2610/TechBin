0c0e99aedb572489a292aee0e726a0522ff60e93
Ranjith k <67511163+ranjith2610@users.noreply.github.com>
2020-07-19 06:17:11 -0700

    Add files via upload
    added chart.dart file

---
+++ lib/chart.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatefulWidget {
+  Chart({this.collection});
+  int collection;
+  @override
+  ChartState createState() => new ChartState();
+}
+
+class ChartState extends State<Chart> {
+  @override
+  Widget build(BuildContext context) {
+    widget.collection == null ? widget.collection = 0 : null;
+    return Scaffold(
+      backgroundColor: Colors.blue[300],
+      body: Container(
+        child: Text("waste thrown is ${widget.collection} kg"),
+      ),
+    );
+  }
+}
