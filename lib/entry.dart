710dc6782f26016d9586143adcdff45143f7c746
Ranjith k <67511163+ranjith2610@users.noreply.github.com>
2020-07-26 02:49:53 -0700

    Update entry.dart
    changed drawer

--- lib/entry.dart
+++ lib/entry.dart
@@ -85,68 +85,261 @@ class _EntryPageState extends State<EntryPage> {
           child: _widgetOptions.elementAt(widget.selectedIndex),
         ),
       ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text('user name'),
-              accountEmail: Text('useremail@gmail.com'),
-              currentAccountPicture: Image(
-                image: AssetImage("lib/image/google_logo.png"),
-              ),
-            ),
-            ListTile(
-              title: new Row(children: [
-                Icon(Icons.notifications),
-                Text('Notificatios'),
-              ]),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-                title: new Row(children: [
-                  Icon(Icons.help),
-                  Text('Help'),
-                ]),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Help(),
+      drawer: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        child: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+
+          child: Container(
+            decoration: BoxDecoration(),
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(122, 219, 141, 1.0),
+                  ),
+                  accountName: Text('Username',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          shadows: [
+                            Shadow(
+                                blurRadius: 10.0,
+                                color: Colors.black38,
+                                offset: Offset(2.0, 2.0))
+                          ])),
+                  accountEmail: Text('useremail@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 15,
+                      )),
+                  currentAccountPicture: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("lib/image/g.png"),
+                  ),
+                ),
+                ListTile(
+                  title: new Row(children: [
+                    Icon(
+                      Icons.notifications_none,
+                      color: Colors.green,
                     ),
-                  );
-                }),
-            ListTile(
-                title: new Row(children: [
-                  Icon(Icons.info_outline),
-                  Text('About Us'),
-                ]),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AbooutUs()),
-                  );
-                }),
-            ListTile(
-              title: new Row(children: [
-                Icon(Icons.exit_to_app),
-                Text('logOut'),
-              ]),
-              onTap: () {
-               
-                googleSignIn == true ? _googlesignOut() : _signOut();
-              },
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                      child: Text(
+                        'Notificatios',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ]),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.history,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'History',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Settings(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.person_outline,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'Profile',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Settings(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.help_outline,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'Help & Feedback',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Help(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.info_outline,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'About Us',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      )
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AbooutUs()),
+                      );
+                    }),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.settings,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'Settings',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Settings(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.share,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'Share app',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Settings(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                  title: new Row(children: [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                      child: Text(
+                        'logOut',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ]),
+                  onTap: () {
+                    googleSignIn == true ? _googlesignOut() : _signOut();
+                  },
+                ),
+                Container(
+                  height: 150,
+                  child: Center(
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(100, 50, 120, 0),
+                      height: 4,
+                      decoration: BoxDecoration(color: Colors.green),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
        floatingActionButton: FloatingActionButton.extended(
